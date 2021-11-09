ad2474bd3e4dbb3465e89add875f9e150db6fb52
Chirag Nagori <chiragnagori8001@gmail.com>
2021-11-09 23:11:53 +0530

    Updates

---
+++ precache-manifest.e487dd1246f1fbe6bb78b77d31780296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3e59ba5bffaa729a6a0",
+    "url": "/Type-Pro/static/css/main.e16a0bdc.chunk.css"
+  },
+  {
+    "revision": "a3e59ba5bffaa729a6a0",
+    "url": "/Type-Pro/static/js/main.4775367d.chunk.js"
+  },
+  {
+    "revision": "7cb2f8725bab59b47af6",
+    "url": "/Type-Pro/static/js/runtime~main.641967ad.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/Type-Pro/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "5238bbdc6e93383ee06ac0ae83aada7f",
+    "url": "/Type-Pro/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Type-Pro/precache-manifest.e487dd1246f1fbe6bb78b77d31780296.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Type-Pro/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
